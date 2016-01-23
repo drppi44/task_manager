995b5232e9efcd7f65c380269591a65c4148795f
nigel <multinigel@gmail.com>
2016-01-23 15:50:52 +0200

    init

---
+++ eugene_shevchenko.py
@@ -0,0 +1,52 @@
+class Task(object):
+    def __init__(self, name, complexity):
+        self.name = name
+        self.complexity = complexity
+        self.bound = False
+
+    def __repr__(self):
+        return '%s  %dcp' % (self.name, self.complexity)
+
+
+class Employee(object):
+    def __init__(self, name, cp_per_time):
+        self.name = name
+        self.cp_per_time = cp_per_time
+        self.tasks = []
+
+    def __repr__(self):
+        return '%s  load=%f' % (self.name, self.load)
+
+    @property
+    def total_cp(self):
+        return sum(task.complexity for task in self.tasks)
+
+    @property
+    def load(self):
+        return self.total_cp / float(self.cp_per_time)
+
+
+class TaskManager(object):
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super(TaskManager, cls).__new__(cls, *args,
+                                                            **kwargs)
+        return cls._instance
+
+    def __init__(self, tasks=list(), employees=list()):
+        self.tasks = tasks
+        self.employees = employees
+
+    def create_empoyee(self, *args, **kwargs):
+        self.employees.append(Employee(*args, **kwargs))
+
+    def create_task(self, *args, **kwargs):
+        self.tasks.append(Task(*args, **kwargs))
+
+    def manage(self):
+        for task in self.tasks:
+            if not task.bound:
+                less_loaded_employee = min(self.employees,
+                                           key=lambda emp: emp.load)
+                less_loaded_employee.tasks.append(task)
+                task.bound = True
---
+++ tests.py
@@ -0,0 +1,144 @@
+from unittest import TestCase
+from eugene_shevchenko import Task, Employee, TaskManager
+from random import randint
+
+
+class TaskTest(TestCase):
+    def test_task_contains_its_data(self):
+        data = {'name': 'anytank', 'complexity': 2}
+        task = Task(**data)
+
+        self.assertEqual(task.name, data['name'])
+        self.assertEqual(task.complexity, data['complexity'])
+        self.assertFalse(task.bound)
+
+    def test_task_could_be_bound(self):
+        data = {'name': 'anytank', 'complexity': 2}
+        task = Task(**data)
+        task.bound = True
+
+        self.assertTrue(task.bound)
+
+
+class EmployeeTest(TestCase):
+    def setUp(self):
+        self.employee_data = {'name': 'anyemployee', 'cp_per_time': 2}
+
+    def test_employee_contains_its_data(self):
+        employee = Employee(**self.employee_data)
+
+        self.assertEqual(employee.name, self.employee_data['name'])
+        self.assertEqual(employee.cp_per_time,
+                         self.employee_data['cp_per_time'])
+        self.assertEqual(employee.tasks, [])
+
+    def test_total_cp_with_empty_tasks(self):
+        employee = Employee(**self.employee_data)
+
+        self.assertEqual(employee.total_cp, 0)
+
+    def test_total_cp_with_tasks(self):
+        tasks = [Task(name='task_%d' % i, complexity=randint(1, 5)) for i in
+                 range(1, 11)]
+
+        employee = Employee(**self.employee_data)
+        employee.tasks = tasks
+
+        self.assertEqual(employee.total_cp,
+                         sum(task.complexity for task in tasks))
+
+    def test_load_with_empty_tasks(self):
+        employee = Employee(**self.employee_data)
+
+        self.assertEqual(employee.load, 0)
+
+    def test_load_with_tasks(self):
+        employee = Employee(**self.employee_data)
+        employee.tasks = [Task(name='task_%d' % i, complexity=randint(1, 5))
+                          for i in range(1, 11)]
+
+        self.assertEqual(employee.load,
+                         employee.total_cp / float(employee.cp_per_time))
+
+
+class ManagerTest(TestCase):
+    def setUp(self):
+        self.max_complexity = 5
+
+    def _create_tasks(self, count):
+        manager = TaskManager()
+        for i in range(count):
+            manager.create_empoyee('employee_%d' % i,
+                                   randint(1, self.max_complexity))
+
+    def _create_employees(self, count):
+        manager = TaskManager()
+        for i in range(10):
+            manager.create_empoyee('employee_%d' % i,
+                                   randint(1, self.max_complexity))
+
+    def _test_load_diff(self):
+        manager = TaskManager()
+        min_load = min(manager.employees, key=lambda emp: emp.load).load
+        for employee in manager.employees:
+            self.assertLessEqual(employee.load - min_load,
+                                 self.max_complexity + 1)
+
+    def test_manager_is_singleton(self):
+        manager1 = TaskManager()
+        manager2 = TaskManager()
+
+        self.assertEqual(manager1, manager2)
+
+    def test_manager_contains_its_data(self):
+        employees = [Employee(name='task_%d' % i, cp_per_time=randint(1, 5))
+                     for i in range(1, 11)]
+        tasks = [Task(name='task_%d' % i, complexity=randint(1, 5)) for i in
+                 range(1, 11)]
+        manager = TaskManager(employees=employees, tasks=tasks)
+
+        self.assertEqual(manager.employees, employees)
+        self.assertEqual(manager.tasks, tasks)
+
+    def test_create_employee(self):
+        data = {'name': 'anyemployee', 'cp_per_time': 2}
+        manager = TaskManager()
+
+        manager.create_empoyee(**data)
+        self.assertEqual(len(manager.employees), 1)
+        self.assertEqual(manager.employees[0].name, data['name'])
+        self.assertEqual(manager.employees[0].cp_per_time, data['cp_per_time'])
+
+    def test_create_task(self):
+        data = {'name': 'anytank', 'complexity': 2}
+        manager = TaskManager()
+
+        manager.create_task(**data)
+        self.assertEqual(len(manager.tasks), 1)
+        self.assertEqual(manager.tasks[0].name, data['name'])
+        self.assertEqual(manager.tasks[0].complexity, data['complexity'])
+
+    def test_manager(self):
+        manager = TaskManager()
+        self._create_employees(10)
+        self._create_tasks(100 * 10)
+        manager.manage()
+
+        self._test_load_diff()
+
+    def test_remanage(self):
+        manager = TaskManager()
+        self._create_employees(10)
+        self._create_tasks(100 * 10)
+        manager.manage()
+        self._test_load_diff()
+
+        self._create_employees(10)
+        self._create_tasks(100 * 10)
+        manager.manage()
+        self._test_load_diff()
+
+        self._create_employees(5)
+        self._create_tasks(100 * 15)
+        self._test_load_diff()
+
